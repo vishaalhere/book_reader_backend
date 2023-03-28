d2919dbf1d2a60f82069e5c7d59176148c6d6fb1
vishaalhere <vishal2001sehgal@gmail.com>
2023-03-28 13:27:52 +0530

    some change

--- dist/database.js
+++ dist/database.js
@@ -1,9 +1,7 @@
 import mysql from "mysql2";
-export const pool = mysql
-    .createPool({
+export const pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DB_NAME,
-})
-    .promise();
+}).promise();
